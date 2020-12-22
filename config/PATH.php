839043ce7ea8c93925e1ef79ca3211cdb8afd25a
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-21 19:59:19 -0600

    sessions PATHs available

---
+++ config/PATH.php
@@ -0,0 +1,11 @@
+<?php 
+  $DS = DIRECTORY_SEPARATOR;
+  $_SESSION['SITE_ROOT'] = dirname(dirname(__FILE__)).$DS;
+  $_SESSION['CONTROLLER_PATH'] = $_SESSION['SITE_ROOT'].'controllers'.$DS;
+  $_SESSION['MODEL_PATH'] = $_SESSION['SITE_ROOT'].'models'.$DS;
+  $_SESSION['VIEW_PATH'] = $_SESSION['SITE_ROOT'].'views'.$DS;
+  $_SESSION['VIEW_PRIVATE_PATH'] = $_SESSION['VIEW_PATH'].'private'.$DS;
+  $_SESSION['VIEW_PUBLIC_PATH'] = $_SESSION['VIEW_PATH'].'public'.$DS;
+  $_SESSION['COMPONENTS_PATH'] = $_SESSION['VIEW_PATH'].'components'.$DS;
+
+?>
