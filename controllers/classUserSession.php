c9a84f78dfe22fd6fd93ad129522b4d79cbc9c53
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2020-12-23 00:30:59 -0600

    change getSession for getSessionData

--- controllers/classUserSession.php
+++ controllers/classUserSession.php
@@ -24,7 +24,7 @@
             return $this->strUser;
         }
 
-        public function getSession(){
+        public function getSessionData(){
             $arrSession = array('User' => $this->strUser);
             return $arrSession;
         }
