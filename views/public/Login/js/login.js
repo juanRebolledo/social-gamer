ffa0d4126d9b0f20e247e3354e76145e79bbba22
Emiliano Vázquez Banda <61950109+McChama@users.noreply.github.com>
2020-12-25 17:34:22 -0600

    Update login.js

---
+++ views/public/Login/js/login.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    for (i=0 ;  i<100 ;i++){
+        const positionLeft = Math.random()*$(document).width();
+        const positionTop = Math.random()*($(document).height())/2;
+        const opacity = Math.random();
+        const parpadeo = Math.random();
+        let objStar;
+        if(parpadeo < 0.3)
+            objStar = `<div class='star animationOne' style='left: ${positionLeft}px; top:${positionTop}px; opacity:${opacity}'> </div>`;
+        else if(parpadeo < 0.6)
+            objStar = `<div class='star animationTwo' style='left: ${positionLeft}px; top:${positionTop}px; opacity:${opacity}'> </div>`;
+        else if(parpadeo < 0.9)
+            objStar = `<div class='star animationThree' style='left: ${positionLeft}px; top:${positionTop}px; opacity:${opacity}'> </div>`;
+        $(".hero-body").append(objStar);
+    }
+})
