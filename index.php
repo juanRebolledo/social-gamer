7018f4096510a8b03857ca69c4cb95617ae6106d
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-25 19:39:24 -0600

    paths are available

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
   session_start();
   include_once('config/PATH.php');
+  include_once("$_SESSION[VIEW_PUBLIC_PATH]". "Login\Login.php");
+  // echo"<br>";
+  // echo("views/public/Login\Login.php");
+  // include_once("views/public/Login\Login.php");
+  // include_once($_SESSION['VIEW_PUBLIC_PATH'].'Login/index.html');
 ?>
