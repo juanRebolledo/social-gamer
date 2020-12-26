595f39f46a47a88f4136353763fd771af3c8e2da
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-25 19:38:52 -0600

    href & source are available from sessions

--- views/public/Login/Login.php
+++ views/public/Login/Login.php
@@ -7,13 +7,11 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 
-    <link rel="stylesheet" href="styles/login.css">
-    <!-- <link rel="stylesheet" href="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]" ?>Login/styles/login.css"> -->
-    <link rel="stylesheet" href="styles/stars.css">
+    <link rel="stylesheet" href="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\styles/login.css" ?>">
+    <link rel="stylesheet" href="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\styles/stars.css" ?>">
     <title>Inicia sesion</title>
 </head>
 <body>
-
     <section class="bg sky">
         <div class="container-login">
             <div class="login-box">
@@ -42,6 +40,6 @@
         </div>
     </section>
 
-    <script src="js/login.js"></script>
+    <script src="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\js/login.js" ?>"></script>
 </body>
 </html>
