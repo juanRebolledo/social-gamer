ecf087757807253065e592f1dffa04284b568959
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-29 01:44:15 -0600

    login view

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
   session_start();
   require_once('config/PATH.php');
   require_once("$_SESSION[VIEW_PUBLIC_PATH]". "Login\Login.php");
+  // require_once("$_SESSION[VIEW_PRIVATE_PATH]". "Home/Home.php");
 ?>
