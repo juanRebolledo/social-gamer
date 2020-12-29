7de2074d35b8b833e44d20522534427b43f2dfea
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-29 01:34:42 -0600

    new paths available

--- config/PATH.php
+++ config/PATH.php
@@ -1,11 +1,13 @@
 <?php 
   $DS = DIRECTORY_SEPARATOR;
   $_SESSION['SITE_ROOT'] = ""; // esto puede quedar unicamente como 'DS'
+  $_SESSION['ASSETS_PATH'] = $_SESSION['SITE_ROOT'].'assets'.$DS;
+  $_SESSION['COMPONENTS_PATH'] = $_SESSION['VIEW_PATH'].'components'.$DS;
   $_SESSION['CONTROLLER_PATH'] = $_SESSION['SITE_ROOT'].'controllers'.$DS;
+  $_SESSION['IMAGES_PATH'] = $_SESSION['ASSETS_PATH'].'img'.$DS;
   $_SESSION['MODEL_PATH'] = $_SESSION['SITE_ROOT'].'models'.$DS;
-  $_SESSION['VIEW_PATH'] = $_SESSION['SITE_ROOT'].'views'.$DS;
   $_SESSION['STYLES_PATH'] = $_SESSION['SITE_ROOT'].'styles'.$DS;
   $_SESSION['VIEW_PRIVATE_PATH'] = $_SESSION['VIEW_PATH'].'private'.$DS;
+  $_SESSION['VIEW_PATH'] = $_SESSION['SITE_ROOT'].'views'.$DS;
   $_SESSION['VIEW_PUBLIC_PATH'] = $_SESSION['VIEW_PATH'].'public'.$DS;
-  $_SESSION['COMPONENTS_PATH'] = $_SESSION['VIEW_PATH'].'components'.$DS;
 ?>
