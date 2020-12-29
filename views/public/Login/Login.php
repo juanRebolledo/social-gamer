301f73926e3ea82058a92a411af9688f9b48a891
Emiliano Vázquez Banda <61950109+McChama@users.noreply.github.com>
2020-12-28 22:23:08 -0600

    Update Login.php

--- views/public/Login/Login.php
+++ views/public/Login/Login.php
@@ -5,41 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">   
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
-
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\styles/login.css" ?>">
     <link rel="stylesheet" href="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\styles/stars.css" ?>">
     <title>Inicia sesion</title>
 </head>
 <body>
     <section class="bg sky">
-        <div class="container-login">
-            <div class="login-box">
-                <div class="login-body">
-                    <div class="container-title">
-                        <h1 class="has-text-centered">Gamer social</h1>
-                    </div>
-
-                    <form action="acciones/validar.php" method="post">
-                        <div class="form-group">
-                            <input autocomplete="off" class="form-control" type="text" placeholder="Usuario" name="usuario" required>
-                            <label class="form-label">Usuario</label>
-                        </div>
-
-                        <div class="form-group">
-                            <input autocomplete="off" class="form-control" type="password" placeholder="Constraseña" name="usuario" required>
-                            <label class="form-label">Constraseña</label>
-                        </div>
-
-                        <div class="has-text-centered">
-                            <button class="button is-fullwidth">Iniciar sesion</button>
-                        </div>
-                    </form>
-                </div>
-            </div>                    
-        </div>
+        <?php
+            if(isset($_GET["register"]))
+                require("$_SESSION[VIEW_PATH]". "components\\register.php");
+            else
+                require("$_SESSION[VIEW_PATH]". "components\login.php");
+        ?>
     </section>
 
-    <script src="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\js/login.js" ?>"></script>
+    <script src="<?php echo "$_SESSION[VIEW_PUBLIC_PATH]". "Login\js/animationStars.js" ?>"></script> 
 </body>
 </html>
