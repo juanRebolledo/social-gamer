5078c3b8e406b3d556b3768f25f5b319239eb373
Emiliano Vázquez Banda <61950109+McChama@users.noreply.github.com>
2020-12-28 22:17:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   session_start();
-  include_once('config/PATH.php');
-  include_once("$_SESSION[VIEW_PUBLIC_PATH]". "Login\Login.php");
+  require_once('config/PATH.php');
+  require_once("$_SESSION[VIEW_PUBLIC_PATH]". "Login\Login.php");
   // echo"<br>";
   // echo("views/public/Login\Login.php");
   // include_once("views/public/Login\Login.php");
