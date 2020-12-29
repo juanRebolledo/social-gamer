ec33a130b9681a6e9171b5e4a8047718c26a5d11
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-28 23:20:59 -0600

    without comments

--- index.php
+++ index.php
@@ -2,8 +2,4 @@
   session_start();
   require_once('config/PATH.php');
   require_once("$_SESSION[VIEW_PUBLIC_PATH]". "Login\Login.php");
-  // echo"<br>";
-  // echo("views/public/Login\Login.php");
-  // include_once("views/public/Login\Login.php");
-  // include_once($_SESSION['VIEW_PUBLIC_PATH'].'Login/index.html');
 ?>
