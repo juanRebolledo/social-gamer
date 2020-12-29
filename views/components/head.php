2fc4d0cd1a9d505232f2386ded4270573f14c493
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-28 23:53:52 -0600

    head that will be used in any file html

---
+++ views/components/head.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">   
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="<?php echo("$_SESSION[STYLES_PATH]"."index.css") ?>">
