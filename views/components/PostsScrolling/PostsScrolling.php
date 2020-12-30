e9100976b32e40e59de3f5590813ebdd576b4740
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2020-12-29 22:52:24 -0600

    comment while infinity

--- views/components/PostsScrolling/PostsScrolling.php
+++ views/components/PostsScrolling/PostsScrolling.php
@@ -15,27 +15,27 @@
   </div>
 </div>
 
-<!-- <?php
-  $count = 1;
-  while(true) {
-    if ($count === 1) {
-      echo "<div class='post b'>$row[src]</div>";
-      $count++;
-    } else if ($count === 2) {
-      echo "<div class='post-s'><div class='post s'>$row[src]</div>";
-      $count++;
-    } else if($count === 3) {
-      echo "<div class='post s'>$row[src]</div></div>";
-      $count++;
-    } else if ($count === 4) {
-      echo "<div class='post-s'><div class='post s'>$row[src]</div>";
-      $count++;
-    } else if($count === 5) {
-      echo "<div class='post s'>$row[src]</div></div>";
-      $count++;
-    } else if ($count === 6) {
-      echo "<div class='post b'>$row[src]</div>";
-      $count = 1;
-    } 
-  }
+<!-- 
+  // $count = 1;
+  // while(true) {
+  //   if ($count === 1) {
+  //     echo "<div class='post b'>$row[src]</div>";
+  //     $count++;
+  //   } else if ($count === 2) {
+  //     echo "<div class='post-s'><div class='post s'>$row[src]</div>";
+  //     $count++;
+  //   } else if($count === 3) {
+  //     echo "<div class='post s'>$row[src]</div></div>";
+  //     $count++;
+  //   } else if ($count === 4) {
+  //     echo "<div class='post-s'><div class='post s'>$row[src]</div>";
+  //     $count++;
+  //   } else if($count === 5) {
+  //     echo "<div class='post s'>$row[src]</div></div>";
+  //     $count++;
+  //   } else if ($count === 6) {
+  //     echo "<div class='post b'>$row[src]</div>";
+  //     $count = 1;
+  //   } 
+  // }
 ?> -->
