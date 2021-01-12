8450d6efd7bf6258372301c4e4c506cf5a7bd67b
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-12 00:29:57 -0600

    Now the page automatically detects the view depending the actual url with out calling a class instance

--- app/Routes.php
+++ app/Routes.php
@@ -1,21 +1,2 @@
 <?php
-
-Route::set('login', function(){
-    Home::CreateView("Login");
-});
-
-Route::set('register', function(){
-    Home::CreateView("Register");
-});
-
-Route::set('create', function(){
-    Home::CreateView("Create");
-});
-
-Route::set('home', function(){
-    Home::CreateView("Home");
-});
-
-Route::set('messages', function(){
-    Home::CreateView('Messages');
-});
+$Route = new Route();
