00722d20d02d49ef6d25b9cc540efbea39e5760b
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-12 00:37:26 -0600

    New mvc url obtaining structure; Now it's possible to get Controller/Method/Params through the url and display the correspondent view automatically. Deleted old mvc structure

--- app/core/Route.php
+++ app/core/Route.php
@@ -1,61 +1,64 @@
 <?php
 class Route{
 
-    protected $controller = "home";
-    protected $method = "index";
-    protected $params = [];
+    public $url;
+    public $controller;
+    public $method;
+    public $params = [];
+    
+    public function __construct(){
+        $this->url = $this->getUrl();
+        $this->controller = $this->getController();
+        $this->method = $this->getMethod();
+        $this->params = $this->getParams();
 
+        $this->callUrl();
+    }
 
-    // public function __construct(){
-    //     $url = $this->parseUrl();
-
-    //     if(isset($url[0])){
-    //         if(file_exists("../app/controllers/" . $url[0] . ".php")){
-    //             $this->controller = $url[0];
-    //             unset($url[0]);
-    //         }
-    //     }
-
-    //     require_once "../app/controllers/" . $this->controller . ".php";
-
-    //     $this->controller = new $this->controller;
-
-    //     if(isset($url[1])){
-    //         if(method_exists($this->controller, $url[1])){
-    //             $this->method = $url[1];
-    //             unset($url[1]);
-    //         }
-    //     }
-
-    //     $this->params = $url ? array_values($url) : [];
-    //     call_user_func_array([$this->controller, $this->method], $this->params);
-    // }
-
-    // public static function parseUrl(){
-    //     /*if(isset($_GET["url"]))
-    //         return $url = explode("/", filter_var(rtrim($_GET["url"], "/"), FILTER_SANITIZE_URL));*/
-    //     return strtolower($_GET['url']);
-    // }
-   // public static $validRoutes = array();
-
-    public static function set($route, $function){
-        //self::$validRoutes[] = $route;
-        
-        
-        if(isset($_GET['url'])){
-            
-            if(strtolower($_GET['url']) == $route){
-                echo $route;
-                $function->__invoke();
-            }
-        }else{
-            $route = "welcome";
-            $function = function(){
-                Home::CreateView("Welcome");
-            };
-            $function->__invoke();
+    public function callUrl(){
+        if(class_exists($this->controller)){
+            $this->controller = new $this->controller;
+            call_user_func_array([$this->controller, $this->method], $this->params);
         }
+        else
+        call_user_func_array(["E404", "indexView"], ["E404"]);
         
+    }
+
+    public static function getUrl(){
+        if(isset($_GET["url"])){
+            $url = explode("/", filter_var(rtrim($_GET["url"], "/"), FILTER_SANITIZE_URL));
+            return $url;
+        }
+    }
+
+    public function getController(){
+        if(isset($this->url[0])){
+                $controller = $this->url[0];
+                unset($this->url[0]);
+                return $controller;
+        }
+        else{
+            $controller = "welcome";
+            return $controller;
+        }
+    }
+
+    public function getMethod(){
+        if(isset($this->url[1])){
+            $method = $this->url[1];
+            unset($this->url[1]);
+            return $method;
+        }
+        else{
+            $method = "indexView";
+            return $method;
+        }
+    }
+
+    public function getParams(){
+        $params = $this->url ? array_values($this->url) : [$this->controller];
+        return $params;
     }
 }
 ?>
