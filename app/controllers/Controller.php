433b8ae661807f9b11dfb0c0e2db10650d79e2fc
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-12 00:33:01 -0600

    New mvc url obtaining structure; Now it's possible to get Controller/Method/Params through the url and display the correspondent view automatically

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,7 +2,7 @@
 
 class Controller{
 
-    public static function CreateView($viewName){
+    public static function indexView($viewName){
         require_once("{$_SERVER['DOCUMENT_ROOT']}/app/views/{$viewName}/{$viewName}.php");
     }
     
