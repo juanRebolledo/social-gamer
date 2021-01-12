269c0dead6c929a62bf0eb7b3eb6753823ce8878
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-11 21:28:52 -0600

    Deleted comments seccion

--- app/Routes.php
+++ app/Routes.php
@@ -1,21 +1,4 @@
 <?php
-// require_once "core/App.php";
-// require_once "core/Controller.php";
-
-// $app = new App();
-
-/*
-/////
-
-require_once('config/system/core/autoload.php');
-use Config\System\Core\AutoLoader;
-use Config\System\Core\PATH;
-
-AutoLoader::loadEverything();
-
-
-
-require_once("{$PATH->VIEW_PUBLIC}Login\Login.php");*/
 
 Route::set('login', function(){
     Home::CreateView("Login");
