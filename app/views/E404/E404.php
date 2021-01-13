7f6cc3cc7142c93705215a284a0368e94a1067c9
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2021-01-12 21:41:20 -0600

    error 404 page finish

--- app/views/E404/E404.php
+++ app/views/E404/E404.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina no encontrada</title>
+<?php
+        $PATH = new PATH(); 
+        require_once("{$PATH->HEAD}head.php") 
+    ?>
+    <link rel="stylesheet" href="<?php echo "{$PATH->CSS}E404.css" ?>">
+    <title>Oh no! ¿Dónde estamos?</title>
 </head>
 <body>
-    <h1>Esta pagina no existe</h1>
+    <div class="app flex-center">
+        <div class="c-error-404">
+            <div class="c-error-about">
+                <h1>Oh no!</h1>
+                <h2>¡Has encontrado un lugar misterioso!</h2>
+                <h2>Entrar fue sencillo, salir será mas.</h2>
+                <a href="/">Regresar</a>
+            </div>
+            <div class="c-error-img">
+                <?php require_once "{$PATH->IMG}e404.svg" ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
