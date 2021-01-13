289ec8f3509b90677596c79d23b323c7718d2ca4
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2021-01-12 20:20:01 -0600

    Autoload is call by a function parsing the current directory location

--- app/init.php
+++ app/init.php
@@ -1,18 +1,8 @@
 <?php
-spl_autoload_register(function($className){
-    $sources = array(
-        $_SERVER["DOCUMENT_ROOT"]."/app/core/".$className.".php",
-        $_SERVER["DOCUMENT_ROOT"]."/app/controllers/".$className.".php"
-    );
-
-    foreach($sources as $source){
-        if(file_exists($source)){
-            require_once $source;
-        }
-    }
-});
-  
+require_once('core/Autoload.php');
+Autoload("/");
 require_once('Routes.php');
+
 
 
 
