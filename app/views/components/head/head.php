25155ad9d90438c2c54fa13260ba39e115eae873
Jose Juan Rebolledo Kamey <juanrekamey1234@gmail.com>
2021-04-07 01:05:47 -0500

    SEO

--- app/views/components/head/head.php
+++ app/views/components/head/head.php
@@ -1,7 +1,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="/<?php echo "{$PATH->IMG}logo/logo.png" ?>" />
 <meta name="theme-color" content="#121212" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">   
+<meta name="description" content="GamerSocial permite la interacción entre distintos usuarios con temática gamer, donde los usuarios pueden crear, editar y eliminar posts de sus juegos favoritos e interactuar con post de distintos usuarios y entonar comunicación entre sí mediante mensajería privada. ">
+<meta name="keywords" content="socialnetwork, GamerSocial, Plataforma GamerSocial" />
+<meta name="author" content="GamerSocial Enterprise">
+<meta property='og:title' content='GamerSocial' />
+<meta property='og:url' content='https://socialgamernetwork2.000webhostapp.com/' />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="GamerSocial permite la interacción entre distintos usuarios con temática gamer, donde los usuarios pueden crear, editar y eliminar posts de sus juegos favoritos e interactuar con post de distintos usuarios y entonar comunicación entre sí mediante mensajería privada. " />
+<meta property="og:site_name" content="GamerSocial" />
+<link rel="shortcut icon" href="/<?php echo "{$PATH->IMG}logo/logo.png" ?>" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="<?php echo("/{$PATH->CSS}index.css"); ?>">
+<link rel="stylesheet" href="<?php echo ("/{$PATH->CSS}index.css"); ?>">
