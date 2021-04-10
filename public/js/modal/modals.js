88d9b8aca05f12dbb9e514aab5db58f964a682f1
Osvaldo Medina <osvaldo.benjamin31@gmail.com>
2021-04-10 18:03:06 -0500

    big modal image

---
+++ public/js/modal/modals.js
@@ -0,0 +1,8 @@
+function openModal() {
+    var modal = document.getElementById("bigModal")
+    modal.style.display='block';
+}
+function closeModal() {
+    var closeBigModal = document.getElementsByClassName("closeBigImage");
+    closeBigModal.style.display='none'
+}
